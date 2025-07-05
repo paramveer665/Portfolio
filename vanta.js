2eb0b6595930af0a1d820ef5fb6b6e38ac19bf8c
paramveer665 <paramveerchauhan665@gmail.com>
2025-07-05 23:15:25 +0530

    added bg

---
+++ vanta.js
@@ -0,0 +1,17 @@
+
+let bg=document.getElementById("vanta-bg")
+
+VANTA.HALO({
+  el: bg,
+   mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  baseColor: 0x3a59,
+  backgroundColor: 0xb00,
+  amplitudeFactor: 0.80,
+  xOffset: 0.17,
+  yOffset: 0.09,
+  size: 0.90
+})
